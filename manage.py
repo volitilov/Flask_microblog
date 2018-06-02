5ee0a9b0dcc3ed86f70bfd95f56cc715d4227b4e
volitilov <volitilov@gmail.com>
2018-06-02 21:10:02 +0300

    feat: добавил тестов для модели user, а также добавил код для измерение степени охвата кода тестированием

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 import os, click
+
 from app import create_app
 from app import db as database
 from app.models.user import User, Follow
@@ -12,11 +13,15 @@ from app.models.role import Role
 from app.models.post import Post
 
 from flask_migrate import Migrate, MigrateCommand
+from dotenv import load_dotenv, find_dotenv, dotenv_values
 
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-app = create_app(os.getenv('FLASK_CONFIG', default='default'))
+load_dotenv(find_dotenv('.env'))
+
+app = create_app(os.getenv('FLASK_CONFIG'))
 migrate = Migrate(app, database)
+
 
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 # команды командной строки
@@ -30,6 +35,32 @@ def test():
     print()
     unittest.TextTestRunner(verbosity=2).run(tests)
     print()
+        
+    subprocess.call('rm -r data_test.sqlite', shell=True)
+
+
+# flask test_cov
+@app.cli.command()
+def test_cov():
+    import coverage, unittest, subprocess
+    cov = coverage.coverage(branch=True, include='app/*')
+    cov.start()
+
+    tests = unittest.TestLoader().discover('tests')
+    print()
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    print()
+    
+    cov.stop()
+    cov.save()
+    print('Coverag Summary:')
+    cov.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir, 'tmp/coverage')
+    cov.html_report(directory=covdir)
+    print('HTML version: file://{}/index.html'.format(covdir))
+    cov.erase()
+        
     subprocess.call('rm -r data_test.sqlite', shell=True)
 
 

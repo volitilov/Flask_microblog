68a871c49ba7e56c642eb7afc2ae1e2511dff2df
volitilov <volitilov@gmail.com>
2018-06-05 16:58:22 +0300

    добавил скрипт для heroku, который позволяет работать с vim

---
+++ vim-heroku.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir ~/vim
+cd ~/vim
+
+# Staically linked vim version compiled from https://github.com/ericpruitt/static-vim
+# Compiled on Jul 20 2017
+curl 'https://s3.amazonaws.com/bengoa/vim-static.tar.gz' | tar -xz
+
+export VIMRUNTIME="$HOME/vim/runtime"
+export PATH="$HOME/vim:$PATH"
+cd -
